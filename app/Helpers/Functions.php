1bee29696686b0375d97810c735b52c3043bbf61
admin@admin.com <admin@admin.com>
2023-08-15 10:46:12 +0900

    fix: 增加try catch 防止在未配置数据库情况下获取setting报错（暂定）

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -3,9 +3,12 @@
 use App\Services\SettingService;
 
 function Setting($name = null, $default = null){
-    if ($name === null) {
-    return app(SettingService::class)->getAll();
+    try{
+        if ($name === null) {
+        return app(SettingService::class)->getAll();
+        }
+        return app(SettingService::class)->get($name, $default);
+    }catch(Exception $e){
+        return null;
     }
-
-    return app(SettingService::class)->get($name, $default);
 }
